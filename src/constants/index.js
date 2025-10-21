163091edd3fdc138c4bd22874307a91e7b88ab69
Catherine Hsu <catherine.hsu.dev@gmail.com>
2025-10-21 19:01:46 +0800

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+// 當在一個folder命名檔名index的時候，表示可以透過/constants/名字,不用寫/constants/index.js
+// import { navLinks } from "../constants"
+export const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "iPhone" },
+  { label: "Watch" },
+  { label: "Vision" },
+  { label: "Airpods" },
+];
